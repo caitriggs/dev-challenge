0a668d420d8bcb73940f8104f868656ccbf0e76f
Caitlin Riggs <caitriggs@gmail.com>
2014-04-03 14:43:42 -0400

    Complete chapter3 exercises

--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,54 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Cait
+
+#Exercise 3.1:
+'''
+NameError: name 'repeat_lyrics' is not defined
+'''
+
+#Exercise 3.2:
+'''
+ABC
+ABC
+ABC
+ABC
+'''
+
+#Exercise 3.3:
+print "Exercise 3.3"
+
+def right_justify(s):
+	print '{:>70}'.format(s)
+	length = "(The length of the text above is %s characters including spaces)" %len(s)
+	print '{:>70}'.format(length)
+
+right_justify('This is right justified to column 70')
+print '' #break line between exerciese
+#source: https://docs.python.org/2/library/string.html#formatspec
+
+#Exercise 3.4:
+#Exercise 3.4.2
+def do_twice(f, printString):
+    f(printString)
+    f(printString)
+
+#Exercise 3.4.3
+def print_twice(printString):
+    print printString
+    print printString
+
+print "Exercise 3.4.4"
+do_twice(print_twice, 'spam')
+print '' #break between next function print
+
+print "Exercise 3.4.5"
+def do_four(f, printString):
+    do_twice(f, printString)
+    do_twice(f, printString)
+
+do_four(print_twice, 'spam')
+print '' #break line between exerciese
+
+
+
